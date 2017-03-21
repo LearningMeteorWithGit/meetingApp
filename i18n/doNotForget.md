bc30ef67ac78b001d57881d594030e19a387ebea
hdammers <hendrik.dammers@ovsoftware.com>
2017-03-21 23:11:40 +0100

    Now including the schema.

---
+++ i18n/doNotForget.md
@@ -0,0 +1,1 @@
+<!-- TODO Implement I18N-->
---
+++ imports/api/editoral/articles.js
@@ -0,0 +1,18 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+export const Articles = new Mongo.Collection('articles');
+
+Articles.schema = new SimpleSchema({
+  title: {
+    type: String,
+    label: 'Article title',
+  },
+  content: {
+    type: String,
+    label: 'Article Content',
+  },
+  // TODO More distinguish*y is needed.
+});
+
+Articles.attachSchema(Articles.schema);
