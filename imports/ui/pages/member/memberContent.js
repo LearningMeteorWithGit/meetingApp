ef92f23abe332b9a13c14c0708971382a57a8873
hdammers <hendrik.dammers@ovsoftware.com>
2017-06-04 20:21:52 +0200

    First step towards chat client

--- imports/ui/pages/member/memberContent.js
+++ imports/ui/pages/member/memberContent.js
@@ -1,1 +1,15 @@
 import './memberContent.html'
+import { Clubs } from '../../../api/clubs/clubs';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+
+Template.memberContent.onCreated(function(){
+  this.state = new ReactiveDict();
+  this.subscribe('clubs');
+})
+
+Template.memberContent.helpers({
+  clubs(){
+    return Clubs.find();
+  }
+});
